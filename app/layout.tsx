68bbebe69fbdeca007b138a9636fad8bc77e9991
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-02 12:53:19 -0600

    update background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-200`}>{children}</body>
+      <body className={`${inter.className} bg-red-50`}>{children}</body>
     </html>
   )
 }
