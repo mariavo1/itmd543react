229d653e20241f2232e2123421d3275cce151238
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-02 15:45:13 -0600

    Change position of links

--- components/header.tsx
+++ components/header.tsx
@@ -9,11 +9,11 @@ export default function Header() {
     </div>
 
     <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul>
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-600 sm:w[inital] sm:flex-nowrap sm:gap-5">
             {
                 links.map((link) => (
-                    <li key={link.hash}>
-                        <Link href={link.hash}>{link.name}</Link>
+                    <li className="h-3/4 flex items-center justify-center" key={link.hash}>
+                        <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-red-300 transition"href={link.hash}>{link.name}</Link>
                     </li>
                 ))
             }
