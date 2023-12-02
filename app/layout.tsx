d2e74d49d8008b5ced4fdd76308ef83b93ab37c4
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-02 13:09:01 -0600

    add z

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-red-50 text-gray-950`}>
-        <div className='bg-yellow-100 absolute top-[-9rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
-        <div className='bg-lime-100 absolute top-[-1rem] left-[-10rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
+      <body className={`${inter.className} bg-red-50 text-gray-950 relative`}>
+        <div className='bg-yellow-100 absolute top-[-9rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
+        <div className='bg-lime-100 absolute top-[-1rem] -z-10 left-[-10rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
         {children}</body>
     </html>
   )
