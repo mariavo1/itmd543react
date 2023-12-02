a9ad0d2cc49df336799a004493de23d055151a5d
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-02 17:17:58 -0600

    Center about me

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-red-50 text-gray-950 relative h-[5000px]`}>
+      <body className={`${inter.className} bg-red-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
         <div className='bg-yellow-100 absolute top-[-9rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
         <div className='bg-lime-100 absolute top-[-1rem] -z-10 left-[-10rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
         
--- components/intro.tsx
+++ components/intro.tsx
@@ -3,12 +3,23 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
         <div className="flex items-center justify-center">
-            <div>
-                <Image src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Picture of computer" width="192" height="192" priority={true}/>
+            <div className='relative'>
+                <Image src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Picture of computer" 
+                width="192" 
+                height="192" 
+                priority={true}
+                className="h-24 w-24 rounded-full object-cover border-[0.30rem] border-white shadow-xl"/>
             </div>
         </div>
+
+        <p className="mb-10 mt-4 px-4 text-1xl font-medium leading-[1.5] sm:text-3xl">
+            Hello, I'm Maria Vo. I am a{" "}
+            student at IIT learning{" "}
+            to become a front-end web developerMy focus is{" "}
+            <span className="underline">HTML, CSS, and JavaScript.</span>
+        </p>
     </section>
   )
 }
