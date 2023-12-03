b95cfd02930e16e0477859ad69de9c3ed0ad6ca1
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-02 18:00:33 -0600

    Change icon size

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,14 +9,14 @@ export default function Intro() {
         <div className="flex items-center justify-center">
             <div className='relative'>
                 <Image src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Picture of computer" 
-                width="192" 
-                height="192" 
+                width="200" 
+                height="200" 
                 priority={true}
-                className="h-24 w-24 rounded-full object-cover border-[0.30rem] border-white shadow-xl"/>
+                className="h-26 w-26 rounded-full object-cover border-[0.30rem] border-white shadow-xl"/>
             </div>
         </div>
 
-        <p className="mb-10 mt-4 px-4 text-1xl font-medium leading-[1.5] sm:text-3xl">
+        <p className="mb-10 mt-4 px-4 text-1xl font-small leading-[1.5] sm:text-3xl">
             Hello, I'm Maria Vo. I am a{" "}
             student at IIT learning{" "}
             to become a front-end web developer. My focus is{" "}
