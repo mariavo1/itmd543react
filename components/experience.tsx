a819e5180fbc9c7345ec4bfa957ce9604f4c32cc
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-05 13:12:12 -0600

    Add paragraph on exp

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SectionHeading from './section-heading';
 
 export default function Experience() {
   return (
-    <div>E</div>
+    <section id="experience" className="scroll-mt-28 mb-28">
+        <SectionHeading>My Experience</SectionHeading>
+        <p>
+            At present time, I am undergoing the process of accumulating more experience.{" "}
+            I will update the section as I do.
+        </p>
+    </section>
   )
 }
