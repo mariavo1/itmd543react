27465884fcfb1e13c46ba2aa5bb26512fc717188
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-05 13:55:12 -0600

    Add footer page

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <small>
+            &copy; 2023 Maria Vo. All rights reserved.
+        </small>
+    </footer>
+  )
+}
