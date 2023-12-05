026e892ef6305291a928e73c181bd63124ea3604
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-05 13:45:06 -0600

    Add required fields

--- components/contact.tsx
+++ components/contact.tsx
@@ -11,8 +11,17 @@ export default function Contact() {
         </p>
 
         <form className="mt-10 flex flex-col">
-            <input className="h-14 rounded-lg px-4" type="email" placeholder="Your email example@gmail.com"/>
-            <textarea className="h-52 my-3 rounded-lg p-4" placeholder="Write your message here"/>
+            <input className="h-14 rounded-lg px-4" 
+            type="email" 
+            required
+            maxLength={200}
+            placeholder="Your email example@gmail.com"/>
+            
+            <textarea className="h-52 my-3 rounded-lg p-4" 
+            required
+            maxLength={500}
+            placeholder="Write your message here"/>
+            
             <button className="bg-pink-200 hover:bg-pink-500 p-4 mt-3 rounded-full text-white outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer" 
             type="submit">Submit</button>
         </form>
