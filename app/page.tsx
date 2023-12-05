bc9d14cdf6a05fe8a65b0b42fef388091fd4f2e6
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-12-05 13:27:18 -0600

    Create form for contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/about";
+import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
@@ -12,6 +13,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Experience />
+      <Contact />
     </main>
   )
 }
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import SectionHeading from './section-heading';
 
 export default function Contact() {
   return (
-    <section></section>
+    <section className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center" id="contact">
+        <SectionHeading>Contact Me</SectionHeading>
+        <p>
+            Contact me directly at <a className="underline" href="mailto:mvo1@hawk.iit.edu">mvo1@hawk.iit.edu</a>{" "}
+            or through this form.
+        </p>
+
+        <form className="mt-10 flex flex-col">
+            <input className="h-14 rounded-lg" type="email" />
+            <textarea className="h-52 my-3 rounded-lg"/>
+            <button className="bg-pink-200" type="submit">Submit</button>
+        </form>
+    </section>
   )
 }
